3c6400367f1b5e8011c93f17225cd0b878565bcc
tacosontitan <contact@tacosontitan.com>
2024-03-12 11:56:27 -0500

    Created extension for member info to determine if the member is obfuscated.

---
+++ src/Asylum.Net/Extensions/MemberInfo/IsObfuscated.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics.Contracts;
+using System.Reflection;
+
+namespace Asylum.Net;
+
+/// <summary>
+/// Defines extension methods for <see cref="MemberInfo"/>.
+/// </summary>
+public static partial class MemberInfoExtensions
+{
+    /// <summary>
+    ///     Determines whether the specified <see cref="MemberInfo"/> is marked with a non-excluded
+    ///     instance of the <see cref="ObfuscationAttribute"/>.
+    /// </summary>
+    /// <param name="source">Specifies the type to check.</param>
+    /// <returns>
+    ///     <see langword="true"/> if the <see cref="MemberInfo"/> is obfuscated;
+    ///     otherwise, <see langword="false"/>.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     The source is null.
+    /// </exception>
+    [Pure]
+    public static bool IsObfuscated(this MemberInfo? source)
+    {
+        if (source is null)
+            throw new ArgumentNullException(nameof(source), "The source type cannot be null.");
+        
+        var obfuscationAttribute = source.GetCustomAttribute<ObfuscationAttribute>();
+        return obfuscationAttribute is { Exclude: false };
+    }
+}
