2ad9cf95169e4bec3f55c864fc410eba3fb4b0fb
tacosontitan <contact@tacosontitan.com>
2024-03-13 09:16:49 -0500

    Added tests for GetName and IsObfuscated.

--- test/Asylum.Net.Tests/Extensions/MemberInfo/GetNameTests.cs
+++ test/Asylum.Net.Tests/Extensions/MemberInfo/GetNameTests.cs
@@ -1,6 +1,64 @@
+using System.Reflection;
+
 namespace Asylum.Net.Tests;
 
+/// <summary>
+/// Tests the <see cref="MemberInfoExtensions.GetName(MemberInfo?)"/> method.
+/// </summary>
+[UsedImplicitly]
+[ExcludeFromCodeCoverage]
 public sealed class GetNameTests
 {
+    [Fact]
+    public void GetName_WhenSourceIsNull_ThrowsArgumentNullException()
+    {
+        MemberInfo? source = null;
+        Assert.Throws<ArgumentNullException>(() => source!.GetName());
+    }
     
+    [Fact]
+    public void GetName_WhenSourceIsNotObfuscated_ReturnsName()
+    {
+        var source = typeof(GetNameExcludedSample);
+        
+        var expectedName = source.Name;
+        var actualName = source.GetName();
+        Assert.Equal(expectedName, actualName);
+    }
+    
+    [Fact]
+    public void GetName_WhenSourceIsObfuscatedWithoutIdentifier_ReturnsName()
+    {
+        var source = typeof(GetNameObfuscatedSample);
+        
+        var expectedName = source.Name;
+        var actualName = source.GetName();
+        Assert.Equal(expectedName, actualName);
+    }
+    
+    [Fact]
+    public void GetName_WhenSourceIsObfuscatedWithIdentifier_ReturnsIdentifier()
+    {
+        var source = typeof(GetNameObfuscatedAndIdentifiedSample);
+        
+        var runtimeIdentifierAttribute = source.GetCustomAttribute<RuntimeIdentifierAttribute>();
+        Assert.NotNull(runtimeIdentifierAttribute);
+        
+        var expectedName = runtimeIdentifierAttribute.Value;
+        var actualName = source.GetName();
+        Assert.Equal(expectedName, actualName);
+    }
+    
+    [UsedImplicitly]
+    [Obfuscation(Exclude = true)]
+    private static class GetNameExcludedSample;
+    
+    [UsedImplicitly]
+    [Obfuscation(Exclude = false)]
+    private static class GetNameObfuscatedSample;
+
+    [UsedImplicitly]
+    [Obfuscation(Exclude = false)]
+    [RuntimeIdentifier(nameof(GetNameObfuscatedAndIdentifiedSample))]
+    private static class GetNameObfuscatedAndIdentifiedSample;
 }
--- test/Asylum.Net.Tests/Extensions/MemberInfo/IsObfuscatedTests.cs
+++ test/Asylum.Net.Tests/Extensions/MemberInfo/IsObfuscatedTests.cs
@@ -1,6 +1,56 @@
+using System.Reflection;
+
 namespace Asylum.Net.Tests;
 
+/// <summary>
+/// Tests the <see cref="MemberInfoExtensions.IsObfuscated(MemberInfo?)"/> method.
+/// </summary>
+[UsedImplicitly]
+[ExcludeFromCodeCoverage]
 public sealed class IsObfuscatedTests
 {
+    [Fact]
+    public void IsObfuscated_WhenSourceIsNull_ThrowsArgumentNullException()
+    {
+        MemberInfo? source = null;
+        Assert.Throws<ArgumentNullException>(() => source!.IsObfuscated());
+    }
     
+    [Fact]
+    public void IsObfuscated_WhenSourceIsNotObfuscated_ReturnsFalse()
+    {
+        var source = typeof(IsObfuscatedUnmarkedSample);
+        
+        var actual = source.IsObfuscated();
+        Assert.False(actual);
+    }
+    
+    [Fact]
+    public void IsObfuscated_WhenSourceIsExcludedWithObfuscationAttribute_ReturnsFalse()
+    {
+        var source = typeof(IsObfuscatedExcludedSample);
+        
+        var actual = source.IsObfuscated();
+        Assert.False(actual);
+    }
+    
+    [Fact]
+    public void IsObfuscated_WhenSourceIsMarkedWithObfuscationAttributeButNotExcluded_ReturnsTrue()
+    {
+        var source = typeof(IsObfuscatedObfuscatedSample);
+        
+        var actual = source.IsObfuscated();
+        Assert.True(actual);
+    }
+    
+    [UsedImplicitly]
+    private static class IsObfuscatedUnmarkedSample;
+    
+    [UsedImplicitly]
+    [Obfuscation(Exclude = true)]
+    private static class IsObfuscatedExcludedSample;
+    
+    [UsedImplicitly]
+    [Obfuscation(Exclude = false)]
+    private static class IsObfuscatedObfuscatedSample;
 }
